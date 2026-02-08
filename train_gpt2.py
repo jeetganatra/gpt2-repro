e3514d2a97a8a7fc7d9b2c74bd2b5c37c53be750
Jeet Ganatra <jeetganatra138@gmail.com>
2026-02-08 22:14:56 +0530

    add TensorFloat32 tf32 matmuls and bf16 support

--- train_gpt2.py
+++ train_gpt2.py
@@ -229,6 +229,7 @@ class DataLoaderLite:
 
 # -----------------------------------------------------------------------------
 # attempt to autodetect the device
+import time
 device = "cpu"
 if torch.cuda.is_available():
     device = "cuda"
@@ -240,7 +241,8 @@ num_return_sequences = 5
 max_length = 100
 
 
-train_loader = DataLoaderLite(B=4, T=32)
+train_loader = DataLoaderLite(B=16, T=1024)
+torch.set_float32_matmul_precision('high')
 
 # model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfig())
@@ -253,13 +255,20 @@ model.to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    logits, loss = model(x, y)
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize() # wait for the GPU to finish work
+    t1 = time.time()
+    dt = (t1 - t0)*1000 # time difference in miliseconds
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
+
 
 import sys; sys.exit(0)
 
